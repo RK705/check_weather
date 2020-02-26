6ca03b624f1c8b3fcf2e3df2c921173a64672bb0
RK705 <31813995+RK705@users.noreply.github.com>
2020-02-26 22:26:20 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // git project https://github.com/erikflowers/weather-icons
 import "weather-icons/css/weather-icons.css";
 
-const Api_Key = "14e7249a4098ac20e7eb6c65fe032f25";
+const Api_Key = process.env.API_KEY;
 
 class App extends React.Component {
   constructor() {
